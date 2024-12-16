9264a8b26bd1b94dc995cc1786a2f48f21d430a7
hix9 <stanislavbarinov0@gmail.com>
2024-12-16 15:10:27 +0300

    task2

--- robots/views.py
+++ robots/views.py
@@ -1,9 +1,18 @@
-from django.http import JsonResponse
+import tempfile
+import csv
+from datetime import timedelta
+from itertools import groupby
+from operator import attrgetter
+from collections import Counter
+
+from django.utils import timezone
+from django.http import JsonResponse, HttpResponse
 from django.views.generic import View
 from django.shortcuts import render
 from django import forms
 
 from .forms import RobotForm
+from .models import Robot
 
 
 class RobotCreate(View):
@@ -41,3 +50,32 @@ class RobotCreate(View):
             status=400,
             json_dumps_params={'ensure_ascii': False}
         )
+
+
+class Report(View):
+    """Генерирует постраничный отчет за неделю в формате CSV."""
+
+    def get(self, request):
+        """
+        Создаёт .csv файл и загружает
+        в директорию для загрузок по умолчанию.
+        """
+        now = timezone.now()
+        one_week_ago = now - timedelta(days=7)
+        robots = Robot.objects.filter(
+            created__gte=one_week_ago
+        ).order_by('model', 'version')
+        with tempfile.NamedTemporaryFile(
+            mode='w+', delete=False, newline='', encoding='utf-8'
+        ) as temp_file:
+            writer = csv.writer(temp_file)
+            for model, group in groupby(robots, key=attrgetter('model')):
+                writer.writerow([f"Модель: {model}"])
+                writer.writerow(["Версия", "Количество"])
+                version_counts = Counter(robot.version for robot in group)
+                writer.writerows(version_counts.items())
+                writer.writerow([])
+            temp_file.seek(0)
+            response = HttpResponse(temp_file.read(), content_type="text/csv")
+            response['Content-Disposition'] = 'attachment; filename="weekly_report.csv"' # noqa
+        return response
